82b16f504a6ac485cdbc49ce173d9d886457f281
Michail Drozdov <michail.drozdov@gmail.com>
2023-08-26 13:49:28 +0300

    Simple response to http://localhost:8000/hello

--- vs-rest/vs-rest/main.cpp
+++ vs-rest/vs-rest/main.cpp
@@ -5,10 +5,28 @@
 
 #pragma comment(lib,"WS2_32")
 
+/**
+ * Custom Request Handler
+ */
+class Handler : public oatpp::web::server::HttpRequestHandler {
+public:
+
+	/**
+	 * Handle incoming request and return outgoing response.
+	 */
+	std::shared_ptr<OutgoingResponse> handle(const std::shared_ptr<IncomingRequest>& request) override {
+		return ResponseFactory::createResponse(Status::CODE_200, "Hello World!");
+	}
+
+};
+
 void run() {
 
 	/* Create Router for HTTP requests routing */
 	auto router = oatpp::web::server::HttpRouter::createShared();
+
+	/* Route GET - "/hello" requests to Handler */
+	router->route("GET", "/hello", std::make_shared<Handler>());
 
 	/* Create HTTP connection handler with router */
 	auto connectionHandler = oatpp::web::server::HttpConnectionHandler::createShared(router);
